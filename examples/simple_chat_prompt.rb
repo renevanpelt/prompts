01c4d273fa8dda85578595c5f649189aaa038fdc
Rene van Pelt <renevanpelt@Renes-MacBook-Pro-2.local>
2023-07-30 02:01:22 +0200

    adds some examples

---
+++ examples/simple_chat_prompt.rb
@@ -0,0 +1,14 @@
+require 'example_helper'
+
+class TranslateToEnglish < Prompts::PromptBuilder
+
+  system 'You are a helpful assistant that translates any text to English.'
+  user 'Translate "hello" to Spanish.'
+  assistant 'This is not the kind of question I am expecting.'
+  user 'Translate "Hola"'
+  assistant 'Hello'
+
+end
+
+translate_prompt = TranslateToEnglish.new
+puts translate_prompt.invoke('Mijn naam is Pieter')
