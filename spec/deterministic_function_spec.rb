08bacf95a541bc285081176747ede72026884067
Rene van Pelt <renevanpelt@Renes-MacBook-Pro-2.local>
2023-07-30 02:13:19 +0200

    adds simple test for deterministic_function

---
+++ spec/deterministic_function_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Prompts::NonDeterministicFunction do
+
+  describe '#type' do
+    it 'returns :non_deterministic' do
+      non_deterministic_function = Prompts::DeterministicFunction.new
+      expect(non_deterministic_function.type).to eq(:deterministic)
+    end
+  end
+
+end
